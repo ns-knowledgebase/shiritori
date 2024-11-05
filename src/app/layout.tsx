1f2dc80840c99c1d305d4ddab330e55dd24a5577
みなぎし <mvxd568.co@icloud.com>
2024-11-05 11:05:28 +0000

    chore: Add tags to prevent indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Shiritori',
   description: 'A web application to help you manage Shiritori',
+  robots: {
+    index: false,
+  },
 };
 
 export const viewport: Viewport = {
